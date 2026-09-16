0e5081d48e7023f1414ce9bf0a36fc96ef96bc07
schackelsz-png <schackelsz@gmail.com>
2026-09-16 09:39:38 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'eod-tracker-v3';
+const CACHE = 'eod-tracker-v3.5';
 const ASSETS = ['./', './index.html', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', e => {
